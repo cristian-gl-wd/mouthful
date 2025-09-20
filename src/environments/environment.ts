b60776095f0798456288aae784f69a1a0dd82e5c
Cristian García López <cristian.gl.wd@gmail.com>
2025-09-20 20:42:23 +0200

    fix(environment): update firebase conf

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyCVpv_RRQbDhgDvDsKsimDEXuCpfEUOu-s',
-    authDomain: 'mouthful-crilop1995-2025.firebaseapp.com',
-    projectId: 'mouthful-crilop1995-2025',
-    storageBucket: 'mouthful-crilop1995-2025.firebasestorage.app',
-    messagingSenderId: '395036206731',
-    appId: '1:395036206731:web:9d0e01273e3cd7390a168e',
+    apiKey: 'AIzaSyAQ5d6KARK_Z8NsuLXu9UCnk9f1MTmSDgs',
+    authDomain: 'mouthful-97fa3.firebaseapp.com',
+    projectId: 'mouthful-97fa3',
+    storageBucket: 'mouthful-97fa3.firebasestorage.app',
+    messagingSenderId: '395494219197',
+    appId: '1:395494219197:web:3448a9a94fcafd3a1b06d4',
+    measurementId: 'G-TL585R22MW',
   },
 };
